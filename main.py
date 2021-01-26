9fdbfece54e69c7f88c172ab4e9a1164f1848309
hoseacodes <dominique11h@yahoo.com>
2021-01-26 11:58:06 -0600

    Players have moevement

--- main.py
+++ main.py
@@ -5,20 +5,26 @@ import time
 
 screen = Screen()
 screen.bgcolor("black")
-screen.setup(width=600, height=600)
-#screen.tracer(0) # delay update
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0) # delay update
 
-player_one = Player()
+player_one = Player((350, 0))
+player_two = Player((-350, 0))
 scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player_one.up, "Up")
 screen.onkey(player_one.down, "Down")
 
+screen.onkey(player_two.up, "w")
+screen.onkey(player_two.down, "s")
+
 
 game_is_on = True
 while game_is_on:
-    player_one.move()
+    screen.update()
+
 
 
 
--- new_player.py
+++ new_player.py
@@ -1,43 +1,24 @@
 from turtle import Turtle
 
-STARTING_POSITION = [(-270, 0), (-270, 20), (-270, 40),  (-270, 60)]
-STARTING_POSITION_TWO = [(270, 0), (270, 20), (270, 40),  (270, 60)]
 up = 90
 down = 270
 move_distance = 20
 
+
 class Player(Turtle):
 
-    def __init__(self):
-        super().__init__
-        self.player_body = []
-        self.create_player()
-        self.head = self.player_body[0]
-
-    def create_player(self):
-        for position in STARTING_POSITION:
-            self.player_shape(position)
-        for position in STARTING_POSITION_TWO:
-            self.player_shape(position)
-
-    def player_shape(self, position):
-        player = Turtle(shape="square")
-        player.penup()
-        player.color("white")
-        player.goto(position)
-        self.player_body.append(player)
-
-    def move(self):
-        for seg in self.player_body:
-            seg.forward(20)
-        #for index in range(len(self.player_body) - 1, -1):
-         #   new_x = self.player_body[index - 1].xcor()
-          #  new_y = self.player_body[index - 1].ycor()
-           # self.player_body[index].goto(new_x, new_y)
-#        self.player_body[0].forward(move_distance)
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.color("white")
+        self.goto(position)
 
     def up(self):
-        self.head.setheading(up)
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
 
     def down(self):
-        self.head.setheading(down)
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
