33ec39ccdf03e161579bee151e3bd92c2acb5e5f
hoseacodes <dominique11h@yahoo.com>
2021-01-26 12:21:26 -0600

    Handle reset

--- main.py
+++ main.py
@@ -25,9 +25,20 @@ screen.onkey(player_two.down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y
 
+    if ball.distance(player_one) < 50 and ball.xcor() > 320 or ball.distance(player_two) < 50 and ball.xcor() > -320 :
+        ball.bounce_x()
 
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
 
 screen.exitonclick()
